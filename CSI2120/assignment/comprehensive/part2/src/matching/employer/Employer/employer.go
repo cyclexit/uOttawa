126f5db2931ad522b7b6c42b1e55c08560036e1d
cyclexit <cyclexit@gmail.com>
2020-02-14 10:32:13 -0500

    Update

--- CSI2120/assignment/comprehensive/part2/src/matching/employer/Employer/employer.go
+++
@@ -1,7 +0,0 @@
-package employer
-
-import "matching/person"
-
-// Employer is a struct to represent employers
-type Employer struct {
-}
--- CSI2120/assignment/comprehensive/part2/src/matching/employer/employer.go
+++ CSI2120/assignment/comprehensive/part2/src/matching/employer/employer.go
@@ -1,1 +1,20 @@
 package employer
+
+import "matching/person"
+
+// Employer is a struct to represent employers
+type Employer struct {
+	p   *person.Person
+	cur int64
+}
+
+// NewEmployer is the constructor of Employer struct
+func NewEmployer(p *person.Person) *Employer {
+	return &Employer{p, 0}
+}
+
+func (e *Employer) GetCurrent() string {
+	name := e.p.GetPref(e.cur)
+	e.cur++
+	return name
+}
