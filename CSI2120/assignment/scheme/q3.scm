d571d9364518fe236c440524b019bd1ea28b3267
cyclexit <cyclexit@gmail.com>
2020-03-29 19:46:56 -0400

    Make some progress

--- CSI2120/assignment/scheme/q3.scm
+++ CSI2120/assignment/scheme/q3.scm
@@ -7,3 +7,32 @@
     ("emma" ("greece" "peru" "vietnam"))
   )
 )
+
+(define (add-places rank places)
+  (cond
+    ( (null? rank) places )
+    ( (equal? #f (member (car rank) places)) (add-places (cdr rank) (append places (list (car rank)))) )
+    ( else (add-places (cdr rank) places) )
+  )
+)
+
+(define (do-add-places choices places)
+  (cond 
+    ( (null? choices) places )
+    ( else (do-add-places (cdr choices) (add-places (cadar choices) places)) )
+  )
+)
+
+(define (assign-pts rank pts)
+  (if (null? rank) 
+    '()
+    (cons (cons (car rank) pts) (assign-pts (cdr rank) (- pts 1)))
+  )
+)
+
+(define (do-assign-pts choices)
+  (if (null? choices)
+    '()
+    (append (assign-pts (cadar choices) 3) (do-assign-pts (cdr choices)))
+  )
+)
