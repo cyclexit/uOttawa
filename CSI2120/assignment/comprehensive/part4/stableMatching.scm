47cf66122fcff78d8ae3773d37c7d1ade711c6a5
cyclexit <cyclexit@gmail.com>
2020-03-30 11:17:09 -0400

    Make some progress

--- CSI2120/assignment/comprehensive/part4/stableMatching.scm
+++ CSI2120/assignment/comprehensive/part4/stableMatching.scm
@@ -9,8 +9,21 @@
   (list-gen (string-split (file->string file) "\n"))
 )
 
+(define (choices-gen emp-pref)
+  (cond
+    ( (null? emp-pref) '())
+    ( else (cons (cons (caar emp-pref) (list 1)) (choices-gen (cdr emp-pref))) )
+  )
+)
+
+(define (stableMatching emp-pref stu-pref)
+  (let ((emp-choices (choices-gen emp-pref)))
+    emp-choices ; test
+  )
+)
+
 (define (findStableMatch emp-file stu-file)
   (let ((emp-pref (read-csv emp-file)) (stu-pref (read-csv stu-file)))
-    emp-pref 
+    (stableMatching emp-pref stu-pref)
   )
 )
