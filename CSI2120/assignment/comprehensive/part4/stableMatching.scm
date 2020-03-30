0f3e88f85293ca894693b0dc29c149317472d5ea
cyclexit <cyclexit@gmail.com>
2020-03-30 13:16:44 -0400

    Make some progress

--- CSI2120/assignment/comprehensive/part4/stableMatching.scm
+++ CSI2120/assignment/comprehensive/part4/stableMatching.scm
@@ -1,3 +1,4 @@
+; input handler ;
 (define (list-gen l)
   (cond
     ( (null? l) '() )
@@ -9,21 +10,76 @@
   (list-gen (string-split (file->string file) "\n"))
 )
 
-(define (choices-gen emp-pref)
+(define (choices-gen emps-pref)
   (cond
-    ( (null? emp-pref) '())
-    ( else (cons (cons (caar emp-pref) (list 1)) (choices-gen (cdr emp-pref))) )
+    ( (null? emps-pref) '())
+    ( else (cons (cons (caar emps-pref) (list 1)) (choices-gen (cdr emps-pref))) )
   )
 )
 
-(define (stableMatching emp-pref stu-pref)
-  (let ((emp-choices (choices-gen emp-pref)))
-    emp-choices ; test
+(define (get-emps emps-pref)
+  (cond
+    ( (null? emps-pref) '())
+    ( else (cons (caar emps-pref) (get-emps (cdr emps-pref))) )
   )
 )
 
-(define (findStableMatch emp-file stu-file)
-  (let ((emp-pref (read-csv emp-file)) (stu-pref (read-csv stu-file)))
-    (stableMatching emp-pref stu-pref)
+; assistant function ;
+(define (emp-match emp match) 
+  (cond
+    ( (null? match) #f )
+    ( (equal? emp (caar match)) (cadar match) ) ; found the matched student
+    ( else (emp-match emp (cdr match)) )
+  )
+)
+
+(define (stud-match stud match)
+  (cond
+    ( (null? match) #f )
+    ( (equal? stud (cadar match)) (caar match) ) ; found the matched employee
+    ( else (stud-match stud (cdr match)) )
+  )
+)
+
+; update the match, 
+; return: the updated match
+; use: evaluate
+(define (offer emp emps-choices emps-pref studs-pref match)
+  (cond
+    (()) ; TODO: here
+  )
+)
+
+(define (solve emps emps-choices emps-pref studs-pref match)
+  (cond
+    ( (null? emps) match )
+    ( else 
+      (solve 
+        (cdr emps) 
+        emps-choices 
+        emps-pref 
+        studs-pref 
+        (offer (car emps) emps-choices emps-pref studs-pref match)
+      ) 
+    )
+  )
+)
+
+; main function
+(define (stableMatching emps-pref studs-pref)
+  (let 
+    ( 
+      (emps (get-emps emps-pref))
+      (emps-choices (choices-gen emps-pref))
+      (match '())
+    )
+    ; emps ; test
+    (solve emps emps-choices emps-pref studs-pref match)
+  )
+)
+
+(define (findStableMatch emp-file stud-file)
+  (let ((emps-pref (read-csv emp-file)) (studs-pref (read-csv stud-file)))
+    (stableMatching emps-pref studs-pref)
   )
 )
