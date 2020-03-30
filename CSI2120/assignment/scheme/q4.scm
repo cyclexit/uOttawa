dc907162dcb1beacd88c9fb5a5b5c57ca7f937d3
cyclexit <cyclexit@gmail.com>
2020-03-30 13:48:42 -0400

    Finish scheme q4 and finish the scheme assignment

--- CSI2120/assignment/scheme/q4.scm
+++ CSI2120/assignment/scheme/q4.scm
@@ -22,3 +22,26 @@
     ( sigmoid (+ 0.5 (+ (* 0.3 (car zs)) (+ (* 0.7 (cadr zs)) (* 0.1 (caddr zs))))) )
   )
 )
+
+(define (calc-x1 k n)
+  (sin (/ (* (* 2 pi) (- k 1)) n))
+)
+
+(define (calc-x2 k n)
+  (cos (/ (* (* 2 pi) (- k 1)) n))
+)
+
+(define (useNet k n)
+  (cond
+    ( (> k n) '() )
+    ( else 
+      (let ((x1 (calc-x1 k n)) (x2 (calc-x2 k n)))
+        (cons (neuralNet (list x1 x2)) (useNet (+ 1 k) n))
+      )
+    )
+  )
+)
+
+(define (applyNet n)
+  (useNet 1 n)
+)
