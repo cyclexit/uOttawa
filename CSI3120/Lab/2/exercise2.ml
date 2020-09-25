765a3d8309f208c883102b08072fbc77dc4a92a3
cyclexit <cyclexit@outlook.com>
2020-09-25 10:50:14 -0400

    Finish the required part of the exercise2.ml

--- CSI3120/Lab/2/exercise2.ml
+++ CSI3120/Lab/2/exercise2.ml
@@ -15,6 +15,14 @@ type employee = string * int * bool
 (* 1. Write a function that takes an employee, and prints out the information in some readable form. *)
 
 let print_employee_info t = 
+    let (name, age, marriage) = t in
+        print_string("Name: " ^ name);
+        print_newline();
+        print_string("Age: " ^ (string_of_int age));
+        print_newline();
+        print_string("Married: " ^ Bool.to_string(marriage));
+        print_newline()
+
 
 (* 2. Reimplement the OCaml standard functions List.length and List.rev.
    This question is optional, but is good practice for the next one. *)
@@ -27,44 +35,69 @@ let rev (l:'a list) : 'a list =
 
 (* 3. Remove the kth element from a list. Assume indexing from 0 *)
 (* example: rmk 2 ["to" ; "be" ; "or" ; "not" ; "to" ; "be"] 
- * results in: [["to" ; "be" ; "not" ; "to" ; "be"] *)
-(* let rmk (k:int) (l:'a list) : 'a list =  *)
+ * results in: ["to" ; "be" ; "not" ; "to" ; "be"] *)
 
+let rec rmk (k:int) (l:'a list) : 'a list = 
+    match l with
+    | [] -> []
+    | elem :: tl -> if k = 0 then tl else elem :: rmk (k-1) tl
 
 (* 4. Write a function that returns the final element of a list, 
    if it exists, and None otherwise *)
-(*
+
 let final (l: 'a list) : 'a option = 
-*)
+    let len = List.length l in
+    if len = 0 then
+        None
+    else
+        Some (List.nth l (len - 1))
 
 (* 5. Write a function to return the smaller of two int options, or None
  * if both are None. If exactly one argument is None, return the other. Do 
  * the same for the larger of two int options.*)
 
-(*
 let min_option (x: int option) (y: int option) : int option = 
-*)
+    match (x, y) with
+    | (None, None) -> None
+    | (a, None) -> a
+    | (None, b) -> b
+    | (a, b) -> min a b
 
-(*
 let max_option (x: int option) (y: int option) : int option = 
-*)
+    match (x, y) with
+    | (None, None) -> None
+    | (a, None) -> a
+    | (None, b) -> b
+    | (a, b) -> max a b
 
 (* 6. Write a function that returns the integer buried in the argument
  * or None otherwise *)  
-(* 
+
 let get_option (x: int option option option option) : int option = 
-*)
+    match x with
+    | None -> None 
+    | Some a -> match a with
+        | None -> None
+        | Some b -> match b with
+            | None -> None
+            | Some c -> c
 
 (* 7. Write a function to return the boolean AND/OR of two bool options,
  * or None if both are None. If exactly one is None, return the other. *)
 
-(*
 let and_option (x:bool option) (y: bool option) : bool option = 
-*)
+    match (x, y) with
+    | (None, None) -> None
+    | (a, None) -> a
+    | (None, b) -> b
+    | (Some a, Some b) -> Some (a && b)
 
-(*
 let or_option (x:bool option) (y: bool option) : bool option = 
-*)
+    match (x, y) with
+    | (None, None) -> None
+    | (a, None) -> a
+    | (None, b) -> b
+    | (Some a, Some b) -> Some (a || b)
 
 (* What's the pattern? How can we factor out similar code? *)
 
