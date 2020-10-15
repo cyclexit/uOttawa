ab6a31d8cf1e6a3f07c434f34bbabd66fa98271c
cyclexit <cyclexit@outlook.com>
2020-10-15 15:52:22 -0400

    Upload review1.ml

---
+++ CSI3120/Tutorial/review1.ml
@@ -0,0 +1,33 @@
+(* For each question in the Brightspace quiz, copy the code and
+   question here, and follow the instructions to answer it.  Do this
+   for all questions except those where you have to draw trees.  (You
+   can do those on a separate piece of paper. *)
+
+(* Question 1: Inductive Data Types
+   (Question 1a can be answered on a separate piece of paper.) *)
+
+
+
+(* Question 2. Polymorphic and Higher-Order Programming *)
+
+
+
+(* Question 3. Parsing and Precedence
+   (All parts of Question 3 can be answered on a separate piece
+   of paper.) *)
+
+
+
+(* Question 4: Anonymous Functions *)
+
+
+
+(* Question 5: More Inductive Data Types *)
+
+
+
+(* Question 6 Imperative Abstract Data Types *)
+
+
+
+(* Question 7: References *)
