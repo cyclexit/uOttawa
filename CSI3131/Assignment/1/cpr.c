5ce87a8115f6cb641afc19dc61e358828acc785a
cyclexit <cyclexit@outlook.com>
2021-05-22 10:54:55 -0400

    Finish coding for cpr.c

--- CSI3131/Assignment/1/cpr.c
+++ CSI3131/Assignment/1/cpr.c
@@ -17,6 +17,7 @@ Explanation of the zombie process
 ------------------------------------------------------------- */
 #include <stdio.h>
 #include <sys/select.h>
+#include <unistd.h>
 
 /* Prototype */
 void createChildAndRead(int);
@@ -63,6 +64,52 @@ Description:
 -------------------------------------------------- ----------- */
 
 void createChildAndRead(int prcNum) {
-/* Please complete this function according to the
-Assignment instructions. */
+	/* Please complete this function according to the
+	Assignment instructions. */
+
+	char msg[BUFSIZ];
+	int msg_len;
+	if (prcNum == 1) {
+		msg_len = sprintf(msg, "Process %d begins\n", prcNum);
+		write(STDOUT_FILENO, msg, msg_len);
+		sleep(5);
+		msg_len = sprintf(msg, "Process %d ends\n", prcNum);
+		write(STDOUT_FILENO, msg, msg_len);
+	} else {
+		msg_len = sprintf(msg, "Process %d begins\n", prcNum);
+		write(STDOUT_FILENO, msg, msg_len);
+
+		// create pipes
+		int pipe_fd[2];
+		if (pipe(pipe_fd) == -1) {
+			fprintf(stderr, "Failed to create pipes\n");
+			return;
+		}
+
+		// create child process
+		int pid = fork();
+		if (pid == -1) {
+			fprintf(stderr, "Failed to fork\n");
+			return;
+		}
+		if (pid == 0) { // child
+			close(pipe_fd[0]);
+			dup2(pipe_fd[1], STDOUT_FILENO);
+			char* file = "./cpr";
+			char pn[20];
+			sprintf(pn, "%d", prcNum - 1);
+			char* cmd[3] = {"cpr", pn, NULL};
+			execvp(file, cmd);
+		} else { // parent
+			close(pipe_fd[1]);
+			char buffer[BUFSIZ];
+			int read_len;
+			while ((read_len = read(pipe_fd[0], buffer, 1)) > 0) {
+				write(STDOUT_FILENO, buffer, read_len);
+			}
+			close(pipe_fd[0]);
+			msg_len = sprintf(msg, "Process %d ends\n", prcNum);
+			write(STDOUT_FILENO, msg, msg_len);
+		}
+	}
 }
