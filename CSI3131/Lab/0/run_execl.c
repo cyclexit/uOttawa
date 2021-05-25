82b862846fa573385ba5ca85f235863abd9f7522
cyclexit <cyclexit@outlook.com>
2021-05-25 17:42:44 -0400

    Remove CSI3131 since it is now in the new repo

--- CSI3131/Lab/0/run_execl.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-
-int main() {
-  char* path = "/bin/ls";
-  char* arg1 = "-lh";
-  char* arg2 = "/home";
-  execl(path, arg1, arg2); // without NULL more info will be printed
-  return 0;
-}
