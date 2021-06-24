e2f4c6e96206f15d0b6db19eaf75c7f2555784a7
cyclexit <cyclexit@outlook.com>
2021-06-23 20:42:57 -0400

    Finish Question 6

--- CSI3140/Assignment/3/A3_Q6_Lin_300053082.js
+++ CSI3140/Assignment/3/A3_Q6_Lin_300053082.js
@@ -1,3 +1,16 @@
+// HTML elements
+const usrInputText = document.getElementById("usr-input");
+const pigLatinText = document.getElementById("pig-latin");
+
+function printLatinWord(word) {
+    pigLatinText.innerHTML += word.substr(1) + word[0] + "ay" + " ";
+    pigLatinText.scrollTop = pigLatinText.scrollHeight;
+}
+
 function pigLatinEncode() {
-    
+    var usrInputArray = usrInputText.value.toString().split(" ");
+    for (var i = 0; i < usrInputArray.length; ++i) {
+        printLatinWord(usrInputArray[i]);
+    }
+    pigLatinText.innerHTML += "\n";
 }
