6c3a6c1158cdf2cd0f3bb921d3627c0edeec27b6
cyclexit <cyclexit@outlook.com>
2022-03-11 22:21:42 -0500

    Update the dat.gui control

--- CSI4130/assignment/1/part_2/desklamp.js
+++ CSI4130/assignment/1/part_2/desklamp.js
@@ -23,57 +23,53 @@ window.addEventListener("resize", () => {
 // 3.2: dat.gui control camera rotation
 const gui = new dat.GUI();
 const cameraRotation = gui.addFolder("Camera Rotation");
-var rotationAngles = {
-    x: 90,
-    y: 90,
-    z: 0
+var sphereCoordinate = {
+    radius: 50,
+    theta: 90, // angle from y positive
+    phi: 0 // angle from x positive on XOY plane
 };
-var rotationRadius = 50;
-camera.position.set(0, 0, rotationRadius);
-function rotateX() {
-    var x = camera.position.x, y= camera.position.y, z = camera.position.z;
-    var angle = rotationAngles.x * Math.PI / 180;
-    camera.position.x = x;
-    camera.position.y = Math.cos(angle) * y - Math.sin(angle) * z;
-    camera.position.z = Math.sin(angle) * y + Math.cos(angle) * z;
+function updateCamera() {
+    var {radius, theta, phi} = sphereCoordinate;
+    theta = theta * Math.PI / 180;
+    phi = phi * Math.PI / 180;
+    camera.position.x = radius * Math.sin(theta) * Math.sin(phi);
+    camera.position.y = radius * Math.cos(theta);
+    camera.position.z = radius * Math.sin(theta) * Math.cos(phi);
     camera.lookAt(0, 0, 0);
 }
-function rotateY() {
-    var x = camera.position.x, y= camera.position.y, z = camera.position.z;
-    var angle = rotationAngles.y * Math.PI / 180;
-    camera.position.x = Math.cos(angle) * x + Math.sin(angle) * z;
-    camera.position.y = y;
-    camera.position.z = -Math.sin(angle) * x + Math.cos(angle) * z;
-    camera.lookAt(0, 0, 0);
-}
-function rotateZ() {
-    var x = camera.position.x, y= camera.position.y, z = camera.position.z;
-    var angle = rotationAngles.z * Math.PI / 180;
-    camera.position.x = Math.cos(angle) * x - Math.sin(angle) * y;
-    camera.position.y = Math.sin(angle) * x + Math.cos(angle) * y;
-    camera.position.z = z;
-    camera.lookAt(0, 0, 0);
-}
-cameraRotation.add(rotationAngles, "x", 0, 360, 1).onChange(rotateX);
-cameraRotation.add(rotationAngles, "y", 0, 360, 1).onChange(rotateY);
-cameraRotation.add(rotationAngles, "z", 0, 360, 1).onChange(rotateZ);
+cameraRotation.add(sphereCoordinate, "radius", 30, 100, 1).listen();
+cameraRotation.add(sphereCoordinate, "theta", 0, 180, 1).listen();
+cameraRotation.add(sphereCoordinate, "phi", 0, 360, 1).listen();
 cameraRotation.open();
 
+// 3.1: create the lamp
+// create 3 sphere joints
+var lampJoins = [];
+for (var i = 0; i < 3; ++i) {
+    lampJoins.push(new THREE.Mesh(
+        new THREE.SphereGeometry(2, 32, 32),
+        new THREE.MeshMatcapMaterial({color: 0xd3e8f8})
+    ));
+}
 
 // add the cylinder base
 const lampBase = new THREE.Mesh(
-    new THREE.CylinderGeometry(10, 10, 1, 32),
+    new THREE.CylinderGeometry(10, 10, 2, 32),
     new THREE.MeshMatcapMaterial({color: 0xd3e8f8})
 );
 scene.add(lampBase);
 
+// add the join 0
+lampJoins[0].position.set(0, 2, 0);
+scene.add(lampJoins[0]);
+
 // add the cylinder lower arm
 const cylinderLowerArm = new THREE.Mesh(
-    new THREE.CylinderGeometry(2, 2, 10, 32),
+    new THREE.CylinderGeometry(2, 2, 15, 32),
     new THREE.MeshMatcapMaterial({color: 0xe4e6e4})
 )
-// cylinderLowerArm.position.set(-20, -30, 0);
-// scene.add(cylinderLowerArm);
+cylinderLowerArm.position.set(0, 8, 0);
+scene.add(cylinderLowerArm);
 
 // add the cylinder upper arm
 const cylinderUpperArm = new THREE.Mesh(
@@ -83,16 +79,7 @@ const cylinderUpperArm = new THREE.Mesh(
 // cylinderUpperArm.position.set(20, -30, 0);
 // scene.add(cylinderUpperArm);
 
-// add 3 sphere joints
-var lampJoins = [];
-for (var i = 0; i < 3; ++i) {
-    lampJoins.push(new THREE.Mesh(
-        new THREE.SphereGeometry(5, 32, 32),
-        new THREE.MeshMatcapMaterial({color: 0xd3e8f8})
-    ));
-    // lampJoins[i].position.set(i * 20, 0, 0);
-    // scene.add(lampJoins[i]);
-}
+
 
 // add the cylinder upper lamp shade
 const cylinderUpperLampShade = new THREE.Mesh(
@@ -121,6 +108,7 @@ const sphereBulb = new THREE.Mesh(
 // start the animation
 function animate() {
     requestAnimationFrame(animate);
+    updateCamera();
     renderer.render(scene, camera);
 }
 animate();
