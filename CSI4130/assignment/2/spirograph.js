2265ccbe3eebdc8b16b3cc6896c3f62b81d1d139
cyclexit <cyclexit@outlook.com>
2022-03-24 19:54:17 -0400

    Implement the my curve class

--- CSI4130/assignment/2/spirograph.js
+++ CSI4130/assignment/2/spirograph.js
@@ -147,3 +147,18 @@ function animate() {
 
 init();
 animate();
+
+class MyCurve extends THREE.Curve {
+    constructor(k, l, r=50) {
+        this.k = k; // inner radius = k * outer radius, 0 < k < 1
+        this.l = l; // pen position
+        this.r = r; // outer radius
+    }
+
+    getPoint(t) {
+        var tx = this.r * ((1 - this.k) * Math.cos(t) + this.l * this.k * Math.cos((1 - k) * t / this.k));
+        var ty = this.r * ((1 - this.k) * Math.sin(t) - this.l * this.k * Math.sin((1 - k) * t / this.k));
+        var tz = 5; // TODO: replace this
+        return new THREE.Vector3(tx, ty, tz);
+    }
+}
