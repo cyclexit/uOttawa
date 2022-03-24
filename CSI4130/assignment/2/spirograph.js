d31f03e103dfc2cbc3a838a72fcf9dd49a8d6884
cyclexit <cyclexit@outlook.com>
2022-03-23 20:49:35 -0400

    Just keep one object and shadow

--- CSI4130/assignment/2/spirograph.js
+++ CSI4130/assignment/2/spirograph.js
@@ -44,23 +44,22 @@ function init() {
     const container = document.getElementById( 'container' );
 
     for ( let i = 0; i < views.length; ++ i ) {
-
         const view = views[ i ];
         const camera = new THREE.PerspectiveCamera( view.fov, window.innerWidth / window.innerHeight, 1, 10000 );
         camera.position.fromArray( view.eye );
         camera.up.fromArray( view.up );
         view.camera = camera;
-
     }
 
+    // create the scene
     scene = new THREE.Scene();
 
+    // add the light
     const light = new THREE.DirectionalLight( 0xffffff );
     light.position.set( 0, 0, 1 );
     scene.add( light );
 
-    // shadow
-
+    // create shadow
     const canvas = document.createElement( 'canvas' );
     canvas.width = 128;
     canvas.height = 128;
@@ -69,63 +68,31 @@ function init() {
     const gradient = context.createRadialGradient( canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2 );
     gradient.addColorStop( 0.1, 'rgba(0,0,0,0.15)' );
     gradient.addColorStop( 1, 'rgba(0,0,0,0)' );
-
     context.fillStyle = gradient;
     context.fillRect( 0, 0, canvas.width, canvas.height );
 
     const shadowTexture = new THREE.CanvasTexture( canvas );
-
     const shadowMaterial = new THREE.MeshBasicMaterial( { map: shadowTexture, transparent: true } );
     const shadowGeo = new THREE.PlaneGeometry( 300, 300, 1, 1 );
 
     let shadowMesh;
-
     shadowMesh = new THREE.Mesh( shadowGeo, shadowMaterial );
     shadowMesh.position.y = - 250;
     shadowMesh.rotation.x = - Math.PI / 2;
     scene.add( shadowMesh );
 
-    shadowMesh = new THREE.Mesh( shadowGeo, shadowMaterial );
-    shadowMesh.position.x = - 400;
-    shadowMesh.position.y = - 250;
-    shadowMesh.rotation.x = - Math.PI / 2;
-    scene.add( shadowMesh );
-
-    shadowMesh = new THREE.Mesh( shadowGeo, shadowMaterial );
-    shadowMesh.position.x = 400;
-    shadowMesh.position.y = - 250;
-    shadowMesh.rotation.x = - Math.PI / 2;
-    scene.add( shadowMesh );
-
+    // add the object
     const radius = 200;
-
     const geometry1 = new THREE.IcosahedronGeometry( radius, 1 );
-
     const count = geometry1.attributes.position.count;
     geometry1.setAttribute( 'color', new THREE.BufferAttribute( new Float32Array( count * 3 ), 3 ) );
 
-    const geometry2 = geometry1.clone();
-    const geometry3 = geometry1.clone();
-
     const color = new THREE.Color();
     const positions1 = geometry1.attributes.position;
-    const positions2 = geometry2.attributes.position;
-    const positions3 = geometry3.attributes.position;
     const colors1 = geometry1.attributes.color;
-    const colors2 = geometry2.attributes.color;
-    const colors3 = geometry3.attributes.color;
-
     for ( let i = 0; i < count; i ++ ) {
-
         color.setHSL( ( positions1.getY( i ) / radius + 1 ) / 2, 1.0, 0.5 );
         colors1.setXYZ( i, color.r, color.g, color.b );
-
-        color.setHSL( 0, ( positions2.getY( i ) / radius + 1 ) / 2, 0.5 );
-        colors2.setXYZ( i, color.r, color.g, color.b );
-
-        color.setRGB( 1, 0.8 - ( positions3.getY( i ) / radius + 1 ) / 2, 0 );
-        colors3.setXYZ( i, color.r, color.g, color.b );
-
     }
 
     const material = new THREE.MeshPhongMaterial( {
@@ -134,27 +101,13 @@ function init() {
         vertexColors: true,
         shininess: 0
     } );
-
     const wireframeMaterial = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true, transparent: true } );
-
-    let mesh = new THREE.Mesh( geometry1, material );
-    let wireframe = new THREE.Mesh( geometry1, wireframeMaterial );
-    mesh.add( wireframe );
-    mesh.position.x = - 400;
-    mesh.rotation.x = - 1.87;
-    scene.add( mesh );
-
-    mesh = new THREE.Mesh( geometry2, material );
-    wireframe = new THREE.Mesh( geometry2, wireframeMaterial );
-    mesh.add( wireframe );
-    mesh.position.x = 400;
-    scene.add( mesh );
-
-    mesh = new THREE.Mesh( geometry3, material );
-    wireframe = new THREE.Mesh( geometry3, wireframeMaterial );
+    mesh = new THREE.Mesh( geometry1, material );
+    wireframe = new THREE.Mesh( geometry1, wireframeMaterial );
     mesh.add( wireframe );
     scene.add( mesh );
 
+    // create the renderer
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
