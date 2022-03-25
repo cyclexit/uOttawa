cb2c3a51fef6dfc2fc8e311dee690eb7b82aa053
cyclexit <cyclexit@outlook.com>
2022-03-25 17:55:38 -0400

    Separate the files

--- CSI4130/assignment/2/spirograph.js
+++ CSI4130/assignment/2/spirograph.js
@@ -27,25 +27,6 @@ const curveGroup = new THREE.Group();
 
 let windowWidth, windowHeight;
 
-// class
-class MyCurve extends THREE.Curve {
-    constructor(k, l, r=20) {
-        super();
-        this.k = k; // inner radius = k * outer radius, 0 < k < 1
-        this.l = l; // pen position
-        this.r = r; // outer radius
-    }
-
-    getPoint(t) {
-        t = t * Math.PI * 6;
-        console.log(t);
-        var tx = this.r * ((1 - this.k) * Math.cos(t) + this.l * this.k * Math.cos((1 - this.k) * t / this.k));
-        var ty = this.r * ((1 - this.k) * Math.sin(t) - this.l * this.k * Math.sin((1 - this.k) * t / this.k));
-        var tz = 5; // TODO: replace this
-        return new THREE.Vector3(tx, ty, tz);
-    }
-}
-
 function init() {
     // add axis helper
     const axesHelper = new THREE.AxesHelper(1000);
